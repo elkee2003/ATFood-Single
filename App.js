7eab9827b269a741dfede216321b611b516227b9
Elkanah <elkee2003@yahoo.com>
2024-06-25 11:29:25 +0100

    6/25/24 update

--- App.js
+++ App.js
@@ -2,8 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/navigation';
 RootNavigator
+// import { Amplify } from 'aws-amplify';
+// import amplifyconfig from './src/amplifyconfiguration.json';
+// Amplify.configure(amplifyconfig);
 
-export default function App() {
+const App = () => {
   return (
   <NavigationContainer>
     <RootNavigator/>
@@ -12,3 +15,5 @@ export default function App() {
     
   );
 }
+
+export default App;
